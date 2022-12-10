08eef1bcae6b53b3b39aee169800bc584361a0e3
SpecialBuilder <specialbuilder32@gmail.com>
2022-12-10 02:26:56 -0500

    Complete Day 9 (refactor decided against)

--- Day 9/Day9.py
+++ Day 9/Day9.py
@@ -2,8 +2,8 @@
 
 # Start Time: 1:28am
 # Pt 1 End Time: 1:53am (24min)
-# Pt 2 End Time: 
-# Total Time: 
+# Pt 2 End Time: 2:26am
+# Total Time: 57min
 
 import numpy as np
 import re
@@ -47,3 +47,29 @@ for line in lines:
 # total visited spaces
 print(f'Total spaces visited by the tail: {len(visited_spaces)}')
         
+# Part 2 - Now we simulate a rope of length 10, composed of 10 unit ropes as simulated above
+    # uhh I was gonna refactor the code above into a function... but its so short I'm not gonna
+knots = 10
+rope = np.zeros((knots,2))
+visited_spaces = set()
+
+for line in lines:
+    dir_id, steps = re.match(r'([RLUD]) (\d+)', line).groups()
+    steps = int(steps)
+    dir_vec = dir_map[dir_id]
+
+    for _ in range(steps): # take step number of iterations
+        rope[0,:] += dir_vec # move global head in direction
+        for H, T in [(rope[i,:], rope[i+1,:]) for i in range(knots-1)]: # for each rope element
+            HT_diff = H-T # vector between head and tail
+            if np.linalg.norm(HT_diff, ord=np.inf) >= 2: # if head is far away to move tail
+                tail_dir = np.clip(HT_diff, -1, 1) # cap motion of taildir
+                T += tail_dir
+
+
+        # update global tail visited list
+        visited_spaces.add( tuple(rope[-1,:]) )
+    # print(f'Rope after instruction {dir_id} {steps} is \n {rope}')
+
+# total visited spaces
+print(f'Total spaces visited by the longer tail: {len(visited_spaces)}')
