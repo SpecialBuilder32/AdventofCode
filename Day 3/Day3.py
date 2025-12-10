c44b0664550eb53333096d0a9196ce7ce4d016e7
SpecialBuilder <specialbuilder32@gmail.com>
2025-12-09 23:27:06 -0500

    Complete Day 3 Pt 2

--- Day 3/Day3.py
+++ Day 3/Day3.py
@@ -2,11 +2,13 @@
 # Day 3 - Battery Joltage!!
 
 # Pt 1 Start Time: 10:52 PM
-# Pt 2 End Time: 10:58 PM (6min 🎉)
+# Pt 1 End Time: 10:58 PM (6min 🎉)
+# Pt 2 End Time: 11:26 PM (28min)
 
 with open("Day 3/Day3_Input.txt", "rt") as f:
     readin = f.readlines()
 
+# PT 1
 total_joltage = 0
 for bank in readin:
     batteries = tuple(map(int, bank.strip()))
@@ -17,5 +19,24 @@ for bank in readin:
 
     joltage = 10*digit1 + digit2
     total_joltage += joltage
-
 print(f"Pt1: Total joltage is {total_joltage}")
+
+# PT 2
+total_joltage = 0
+for bank in readin:
+    batteries = tuple(map(int, bank.strip()))
+    digits = [None]*12
+
+    start_idx = -1
+    for i in range(12): # for each of the 1 batteries
+        end_idx = len(batteries)-(12-i) # don't check the last 12-ith elements, we to still have 12 batteries total
+        searchable_digits = batteries[start_idx+1:end_idx+1]
+        digits[i] = max(searchable_digits)
+        start_idx = searchable_digits.index(digits[i])+start_idx+1
+    
+    joltage = 0
+    for pow, dig in enumerate(reversed(digits)): # convert to value
+        joltage += dig*10**pow
+
+    total_joltage += joltage
+print(f"Pt2: Total joltage is {total_joltage}")
