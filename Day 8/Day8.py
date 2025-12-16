bae30eac861c1f350d0e34097cafb9311f9aca6f
SpecialBuilder32 <specialbuilder32@gmail.com>
2025-12-16 14:35:47 -0500

    Complete Day 8 Pt 2

--- Day 8/Day8.py
+++ Day 8/Day8.py
@@ -5,6 +5,8 @@
     # 9:53 AM - 11:22 AM; still debugging
     # 2:10 PM - 2:19 PM; I only updated x or y's membership list, not the whole set that is getting merged
     # Total time: 2hr 10min
+# Pt 2 Start Time: 2:19 PM
+# Pt 2 End Time: 2:35 PM (16min)
 
 import numpy as np
 from math import prod
@@ -34,25 +36,31 @@ print("..junction boxes sorted")
 
 # go through list in distance order and connect junction boxes into the network/web
 max_connections = 1000
-networks = {} # {network_id: set(junc_idx)}
-network_membership = {} # {junc_idx: network_id}
-new_network_id = 1000
+networks = {x:{x} for x in range(len(juncboxes))} # {network_id: set(junc_idx)}
+network_membership = {x:x for x in range(len(juncboxes))} # {junc_idx: network_id}
+new_network_id = len(juncboxes)+1000
 for x,y in zip(*indx_incr_distance):
     x = int(x)
     y = int(y)
 
-    if distances_matrix[x,y] == np.inf or max_connections <= 0:
-        break # we've processed all the boxes, quit
+    if distances_matrix[x,y] == np.inf: # no more boxes to process
+        break
+
+    if max_connections == 0: # we've finished for part 1
+        netwk_sizes = sorted(map(len, networks.values()), reverse=True)
+        pt1_answer = prod(netwk_sizes[0:3])
+        print(f"Pt1: Product of 3 largest circuits is {pt1_answer}")
 
     # print(f"box {juncboxes[x]}({x}) is closest to {juncboxes[y]}({y}); dist={distances_matrix[x,y]:0.2f}")
 
 
-    if x not in network_membership and y not in network_membership:
-        # new standalone segment
-        networks[new_network_id] = {x,y}
-        network_membership[x] = new_network_id
-        network_membership[y] = new_network_id
-        new_network_id += 1
+    # if x not in network_membership and y not in network_membership:
+    #     # new standalone segment
+    #     networks[new_network_id] = {x,y}
+    #     network_membership[x] = new_network_id
+    #     network_membership[y] = new_network_id
+    #     new_network_id += 1
+    ## Pt 2 makes all operations a merge
 
     # elif network_membership.get(x) == network_membership.get(y):
     #     # these boxes are already connected via other connections, skip!
@@ -67,27 +75,30 @@ for x,y in zip(*indx_incr_distance):
 
     else:
         # we merge existing segments
-        netxid = network_membership.get(x, -1) # if x is not connected, treat it as a network of one element (-1 will not be a valid id)
-        netx = networks.get(netxid, {x})
+        netxid = network_membership[x]
+        netx = networks[netxid]
 
-        netyid = network_membership.get(y, -1)
-        nety = networks.get(netyid, {y})
+        netyid = network_membership[y]
+        nety = networks[netyid]
 
-        merge_into_id = netxid if netxid > -1 else netyid
-        del_id = netyid if netxid > -1 else netxid
+        merge_into_id = netxid # always merge onto the x segment
+        del_id = netyid
         networks[merge_into_id] = netx|nety
         network_membership[x] = network_membership[y] = merge_into_id
         for connected_id in networks.get(del_id,{}):
             network_membership[connected_id] = merge_into_id
         networks.pop(del_id, None)
         
+    if len(networks) == 1: # only one network! pt2 done
+        print(f"Last two boxes to connect are {juncboxes[x]}, {juncboxes[y]}")
+        pt2_answer = juncboxes[x][0] * juncboxes[y][0]
+        print(f"Pt2: product of x coords is {pt2_answer}")
+        break
+    
     # print(networks)
     max_connections -= 1
 
-# print(networks)
-netwk_sizes = sorted(map(len, networks.values()), reverse=True)
-pt1_answer = prod(netwk_sizes[0:3])
-print(f"Pt1: Product of 3 largest circuits is {pt1_answer}")
+
 
 # check solution
     # each number should show up in exactly one set
